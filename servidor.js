e5813d2b8079381267c855c167fe78636b572f39
E25llin <143622878+E25llin@users.noreply.github.com>
2024-07-17 12:17:12 -0300

    Aula 17/07

--- servidor.js
+++ servidor.js
@@ -7,6 +7,38 @@ app.get('/conta', (request, response) => {
    response.render('conta')
 })
 
+app.get('/calu', (request, response) => {
+   response.render('calculadora')
+})
+
+app.get('/lua', (request, response) => {
+   response.render('calculadora', {resultado: ''})
+})
+
+app.get('/soma', (request, response) => {
+   soma = parseFloat(request.query.n1) + parseFloat(request.query.n2);
+   response.render('calculadora', {resultado: soma})
+   
+})
+
+app.get('/subt', (request, response) => {
+   subt = parseFloat(request.query.n1) - parseFloat(request.query.n2);
+   response.render('calculadora', {resultado: subt})
+    
+})
+
+app.get('/multi', (request, response) => {
+   multi = parseFloat(request.query.n1) * parseFloat(request.query.n2);
+   response.render('calculadora', {resultado: multi})
+
+})
+
+app.get('/divi', (request, response) => {
+   soma = parseFloat(request.query.n1) / parseFloat(request.query.n2);
+   response.render('calculadora', {resultado: divi})
+   
+})
+
 const PORT = 8000
 app.listen(PORT, () => {
    console.log("Servidor rodando");
